6cdc1e9127503a654ac9c5486e7e07707a7e8504
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-10 22:10:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int i=0, n=0, p=i, s=1, f=0;
+    cin >>n;
+    if (n>=2) {
+        cout << "1" << endl;
+        for (int i=2; i<=n; i++) {
+            f = p + s;
+            p = s;
+            s = f;
+            cout << f << endl;
+        }
+    }else{
+        cout << "errore";
+    }
+    return 0;
 }
